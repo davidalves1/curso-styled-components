68d8931f056ff3407221ea2e33547e4ca4bb6a4c
davidalves1 <alves.david@outlook.com>
2019-11-25 13:17:21 -0300

    Initial commit

---
+++ src/components/common/Button.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const Button = styled.button`
+  padding: 10px;
+  background: #efefef;
+  border: 1px solid #333;
+  font-size: 20px;
+  color: #333;
+  cursor: pointer;
+`;
+
+export default Button;
